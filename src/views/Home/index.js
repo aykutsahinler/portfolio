bc09b602f91635d3a9687696f6a04bbd9f879e64
aykutsahinler <aykut-sahinler@hotmail.com>
2022-02-24 23:15:39 +0300

    add navigate / to /resume

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,8 +1,17 @@
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
 import Navbar from "../../components/Navbar";
 import styles from "./styles.module.css";
 
 const Home = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (window.location.pathname === "/") {
+      navigate("/resume");
+    }
+  }, [navigate]);
+
   return (
     <div className={styles.container}>
       <Navbar />

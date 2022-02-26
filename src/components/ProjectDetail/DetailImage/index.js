cd8d0a6b6b6ebb0eec5cf00474a8147f89af8e83
aykutsahinler <aykut-sahinler@hotmail.com>
2022-02-26 21:47:21 +0300

    add close icon to detail image

--- src/components/ProjectDetail/DetailImage/index.js
+++ src/components/ProjectDetail/DetailImage/index.js
@@ -1,10 +1,19 @@
-import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { useNavigate } from "react-router-dom";
 import "./styles.css";
 
 const DetailImage = ({ image }) => {
+  const navigate = useNavigate();
   return (
-    <div>
+    <div className="mainContainer">
       <img alt="imageDesc" loading="lazy" src={image} className="image" />
+      <div className="close" onClick={() => navigate(-1)}>
+        <div className="close-container">
+          <span className="cross-line top-left"></span>
+          <span className="cross-line top-right"></span>
+          <span className="cross-line bottom-left"></span>
+          <span className="cross-line bottom-right"></span>
+        </div>
+      </div>
     </div>
   );
 };
